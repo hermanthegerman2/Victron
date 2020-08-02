be093f9da500171fc45278fc924314538f018fc9
hermanthegerman2 <doetsch.hermann@t-online.de>
2020-08-03 01:39:57 +0200

    Form

--- VEDirect/module.php
+++ VEDirect/module.php
@@ -421,8 +421,8 @@ require_once __DIR__ . "/../libs/ModuleHelper.php";
                                     }
                                 }
                             }
-                            $id = implode ($this->_getIdentifierByNeedle($parent_id, $needle));
-                            $this->SendDebug("Schreiben id ", $id." : value: ".$value, 0);
+                            $id[] = $this->_getIdentifierByNeedle($parent_id, $needle);
+                            //$this->SendDebug("Schreiben id ", $id." : value: ".$value, 0);
                     }
 
                 }
