76f3136946884d05781d06eae0e3639c65d0da14
hermanthegerman2 <doetsch.hermann@t-online.de>
2020-08-03 11:35:40 +0200

    Form

--- VEDirect/module.php
+++ VEDirect/module.php
@@ -363,7 +363,7 @@ require_once __DIR__ . "/../libs/ModuleHelper.php";
                         }
                     }
                     else {
-                        $this->SendDebug("Keine id !!!",$id." divisor: ".$divider." : value: ".$labelvalue, 0);
+                        $this->SendDebug("Keine Variable angelegt !!!",$label." divisor: ".$divider." : value: ".$labelvalue, 0);
                     }
 
                 }
