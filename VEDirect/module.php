ebab02a6dca7164bccab9c19e2e0c10366927553
hermanthegerman2 <doetsch.hermann@t-online.de>
2020-08-03 16:20:41 +0200

    Form

--- VEDirect/module.php
+++ VEDirect/module.php
@@ -334,11 +334,11 @@ require_once __DIR__ . "/../libs/ModuleHelper.php";
                                             }
                                         }
                                         $ident = $this->InstanceID . '_' . $value['Name'];
-                                        $custom_profile = isset($value['custom_profile']) && $value['custom_profile'] ? $value['custom_profile'] : false;
+                                        $custom_profile = isset($v['custom_profile']) && $v['custom_profile'] ? $v['custom_profile'] : false;
 
                                         $this->CreateVariableByIdentifier([
                                             'parent_id' => $this->InstanceID,
-                                            'name' => $value['Name'],
+                                            'name' => $v['Name'],
                                             'value' => $v['Value'],
                                             'identifier' => $ident,
                                             'position' => $position,
