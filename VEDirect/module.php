b3fb976f74f74948780b512a5261daa49ab552d1
hermanthegerman2 <doetsch.hermann@t-online.de>
2020-08-04 23:12:51 +0200

    Form

--- VEDirect/module.php
+++ VEDirect/module.php
@@ -329,14 +329,14 @@ require_once __DIR__ . "/../libs/ModuleHelper.php";
                         // Prüfung ob instance_id gesetzt ?
                         if (empty($this->ReadAttributeInteger('instance_id'))) {
                             $this->WriteAttributeInteger('instance_id', $this->InstanceID);
-                            $this->_log("Victron Gerät gefunden: ", $PID,true);
+                            $this->senddebug("Victron Gerät gefunden: ", $PID,0);
 
                             // Gerätevariablen anlegen
 
                             $position = 0;
                             foreach ($this->variable_mapping as $key => $value) {
                                 $DisplayedValues = $this->device_mapping[$PID]['DisplayedValues'];
-                                $this->_log("Victron Gerät gefunden: ", json_encode($DisplayedValues),true);
+                                $this->senddebug("Victron Gerät gefunden: ", json_encode($DisplayedValues),0);
                                 if (in_array($key, $DisplayedValues) == true) {         // ist die Variable im Array display_mapping dabei ?
                                     $ident = $this->InstanceID . '_' . $value['Name'];
                                     $custom_profile = isset($value['custom_profile']) && $value['custom_profile'] ? $value['custom_profile'] : false;
